63cb34696bf1acf26844a28cec576a1c99faa58a
mahmoudgray <dm007830@etu.unice.fr>
2015-12-04 19:14:36 +0100

    Ajout d'icone intuitive

--- mediastore-rest/src/main/webapp/mediastore/common/directives/folderDirective.js
+++ mediastore-rest/src/main/webapp/mediastore/common/directives/folderDirective.js
@@ -5,6 +5,6 @@ var app = angular.module('folders.directive', []);
 app.directive('mediaFolder', [function() {
 	
 	return {
-		template: '<div id="db_paths" style="margin-bottom: 0"  ng-repeat="p in paths"><label><input type="checkbox" ng-model="path" name="folder" checked="true" value="{{p}}">{{p}}</label><br></div>'
+		template: '<div id="db_paths" style="margin-bottom: 0"  ng-repeat="p in paths"><label><input type="checkbox" ng-model="path" name="folder" checked=true value="{{p}}">{{p}}</label><br></div>'
 	};
 }]);
--- mediastore-rest/src/main/webapp/mediastore/common/services/thumbnailService.js
+++ mediastore-rest/src/main/webapp/mediastore/common/services/thumbnailService.js
@@ -8,3 +8,10 @@ service.factory('ThumbnailFactory', [function() {
 	
 
 }]);
+
+service.factory('ThumbnailsFactory', [function(){
+
+
+
+
+}]);
