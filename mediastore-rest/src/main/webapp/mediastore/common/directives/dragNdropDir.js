4bc8f70b17a720a3901d12aa5dc640c14901ad6e
Mohannad GHINNAGOW <mohannadgen@gmail.com>
2016-02-20 17:54:51 +0100

    added js files

---
+++ mediastore-rest/src/main/webapp/mediastore/common/directives/dragNdropDir.js
@@ -0,0 +1,13 @@
+(function () {
+	// body...
+	var app = angular.module('dragndrop.dir', []);
+
+	app.directive('dragNdrop', ['$http', function($http){
+		return {
+			restrict: 'A',
+			link: function(scope, element, attrs){
+
+			};
+		};
+	}]);
+})();
---
+++ mediastore-rest/target/mediastore-rest-1.0/mediastore/common/services/thumbnailService.js
@@ -0,0 +1,17 @@
+/**
+ * Created by mahmoud on 29/11/2015.
+ */
+
+var service = angular.module('thumbnail.service', []);
+
+service.factory('ThumbnailFactory', [function() {
+	
+
+}]);
+
+service.factory('ThumbnailsFactory', [function(){
+
+
+
+
+}]);
